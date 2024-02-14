e87d9f3dbf8ca36065204e27c89ecef0e7767cb7
cppmonger <keskinemirhan@gmail.com>
2024-02-14 12:42:56 +0300

    Add some preferences

--- init.lua
+++ init.lua
@@ -10,4 +10,18 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- vim.cmd("colorscheme rasmus")
 vim.cmd("colorscheme catppuccin")
 -- vim.cmd("colorscheme moonfly")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd('let &t_SI = \"\\e[6 q\"')
+vim.cmd('let &t_EI = \"\\e[2 q\"')
+
+
+
+
+
+
+
+
 
