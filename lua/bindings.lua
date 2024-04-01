086b24934c96643a59573cf8c42f345de2b0a8cf
cppmonger <keskinemirhan@gmail.com>
2024-04-01 18:13:21 +0300

    Add tab and window bindings

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -2,6 +2,35 @@ local cmp = require("cmp")
 
 vim.g.mapleader = " "
 
+-- Windows 
+vim.keymap.set('n', '<leader>r', '<cmd>vsplit<cr>')
+vim.keymap.set('n', '<leader>p', '<cmd>split<cr>')
+
+-- navigating windows
+vim.cmd('nnoremap <leader>h <C-w>h')
+vim.cmd('nnoremap <leader>j <C-w>j')
+vim.cmd('nnoremap <leader>k <C-w>k')
+vim.cmd('nnoremap <leader>l <C-w>l')
+
+-- Tabs
+vim.keymap.set('n', '<leader>n', '<cmd>tabnext<cr>')
+vim.keymap.set('n', '<leader>c', '<cmd>tabnew<cr>')
+vim.keymap.set('n', '<leader>b', '<cmd>tabprevious<cr>')
+
+-- navigating tabs
+
+vim.keymap.set('n', '<leader>1', '<cmd>tabn 1<cr>')
+vim.keymap.set('n', '<leader>2', '<cmd>tabn 2<cr>')
+vim.keymap.set('n', '<leader>3', '<cmd>tabn 3<cr>')
+vim.keymap.set('n', '<leader>4', '<cmd>tabn 4<cr>')
+vim.keymap.set('n', '<leader>5', '<cmd>tabn 5<cr>')
+vim.keymap.set('n', '<leader>6', '<cmd>tabn 6<cr>')
+vim.keymap.set('n', '<leader>7', '<cmd>tabn 7<cr>')
+vim.keymap.set('n', '<leader>8', '<cmd>tabn 8<cr>')
+vim.keymap.set('n', '<leader>9', '<cmd>tabn 9<cr>')
+vim.keymap.set('n', '<leader>0', '<cmd>tabn 10<cr>')
+
+
 -- Lsp diagnostic
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
@@ -15,11 +44,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- navigating windows
-vim.cmd('nnoremap <C-h> <C-w>h')
-vim.cmd('nnoremap <C-j> <C-w>j')
-vim.cmd('nnoremap <C-k> <C-w>k')
-vim.cmd('nnoremap <C-l> <C-w>l')
 
 -- plugin toggling 
 vim.cmd('nnoremap <leader>o :NvimTreeToggle<CR>')
